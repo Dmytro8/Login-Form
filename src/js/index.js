c36f8792fa2e311233dfbce7c901cfea9e20de0a
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-06-08 01:06:01 +0200

    first task was done

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
 /* Here goes your JS code */
+const popup = document.getElementById("popup");
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const buttonOpenForm = document.getElementById("show-popup-form");
+const iconCloseForm = document.getElementById("icon-close-form");
+
+buttonOpenForm.addEventListener("click", (e) => {
+  popup.style.opacity = "1";
+  popup.style.visibility = "visible";
+  buttonOpenForm.style.opacity = "0";
+  buttonOpenForm.style.visibility = "hidden";
+});
+iconCloseForm.addEventListener("click", (e) => {
+  popup.style.opacity = "0";
+  popup.style.visibility = "hidden";
+  buttonOpenForm.style.opacity = "1";
+  buttonOpenForm.style.visibility = "visible";
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
